db2c9a5b70e7fc535ffd913d214d2631c525da38
khahhan <khanhanbui1010@gmail.com>
2024-12-15 11:44:08 +0700

    up code

--- src/plugins/webSocket.js
+++ src/plugins/webSocket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
-const socketConnect = io('https://your-new-socket-server-url', {
+const socketConnect = io('https://whatsapp-server-lemon.vercel.app/', {
   autoConnect: false,
   transports: ['websocket'], // Chỉ sử dụng websocket để tránh lỗi fallback
   secure: true
