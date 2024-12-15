4dedb9f2ee685befbd1a9a5e8669bca510bc6dd3
khahhan <khanhanbui1010@gmail.com>
2024-12-15 11:48:03 +0700

    up code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,23 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      // Proxy HTTP requests
+      '/api': {
+        target: 'https://whatsapp-server-lemon.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix
+      },
+      // Proxy WebSocket requests
+      '/socket.io': {
+        target: 'https://whatsapp-server-lemon.vercel.app',
+        ws: true, // Enable WebSocket proxying
+        changeOrigin: true,
+      },
+    },
+  },
+});
